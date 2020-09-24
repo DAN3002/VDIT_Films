59e0208d1090d07ee175d301d8aa15a454111a8a
dan3002 <anhndfx00424@funix.edu.vn>
2020-09-24 20:33:31 +0700

    Deploy heroku

--- src/startup/firebaseSetup.js
+++
@@ -1,9 +0,0 @@
-import admin from 'firebase-admin';
-import * as serviceAccount from '../serviceAccount.json'
-
-export function firebaseSetup() {	
-	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount),
-		databaseURL: "https://vdit-films.firebaseio.com"
-	 });
-}
