1c8ee9412da17affb528abac0b878590ec73ee68
dan3002 <anhndfx00424@funix.edu.vn>
2020-09-24 15:39:22 +0700

    Add search

---
+++ src/libs/FilmCrawl.js
@@ -0,0 +1,35 @@
+import Crawl from './Crawl';
+
+const FilmCrawl = {
+	async getFilmFromName(name) {
+		const url = `http://www.phimmoizz.net/tim-kiem/${name}/`
+		const $ = await Crawl.parseDOMFromURL(url);
+
+		const films = $('li.movie-item');
+
+		const out = [];
+
+		for(let i = 0; i < films.length; i++) {
+			const el = $(films[i]);
+
+			const title = el.find('.movie-title-1').text();
+			const name = el.find('.movie-title-2').text();
+			const time = el.find('.movie-title-chap').text();
+			const link = el.find('a.block-wrapper').attr('href')
+								.replace('phim/', '')
+								.replace('/', '');
+
+			const image = el.find('.movie-thumbnail').attr('style')
+							.match(/\(.+\)/g)[0]
+							.replace(/[\(\)]/g, '');
+
+			out.push({
+				title, name, time, image, link
+			})
+		}
+
+		return out;
+	}
+}
+
+export default FilmCrawl;
