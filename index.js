3720e92ec74ecef84032f20627cf8ef031a51ddb
dan3002 <anhndfx00424@funix.edu.vn>
2020-09-24 21:05:38 +0700

    Remove firebase functions

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 
-// const firebaseSetup = require('./startup/firebaseSetup');
-// const filmsRouter = require('./routers/filmRouter');
+const firebaseSetup = require('./startup/firebaseSetup');
+const filmsRouter = require('./routers/filmRouter');
 
 const app = express();
-// firebaseSetup();
+firebaseSetup();
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.use(filmsRouter);
 
-app.get('/', (req, res) => {
-	res.send("asdASDASD");
-});
+// app.get('/', (req, res) => {
+// 	res.send("asdASDASD");
+// });
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// app.set('view engine', 'ejs');
-// app.use(filmsRouter);
 
 // app.listen(3000);
 // exports.app = functions.https.onRequest(app);
