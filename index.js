ba0fcff5c640d4514158d21f237b5d13ba4f0193
dan3002 <anhndfx00424@funix.edu.vn>
2020-09-25 15:04:46 +0700

    Add comment

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 
 const firebaseSetup = require('./startup/firebaseSetup');
+firebaseSetup();
+
 const filmsRouter = require('./routers/filmRouter');
 
 const app = express();
-firebaseSetup();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(filmsRouter);
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- routers/filmRouter.js
+++ routers/filmRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const FilmCrawl = require('../libs/FilmCrawl');
+const Film = require('../models/Film');
 
 const router = express.Router();
 
@@ -13,6 +14,8 @@ router.get('/', (req, res) => {
 
 router.get('/:search', async (req, res) => {
 	const { search } = req.params;
+	console.log('search', search);
+
 	const list = await FilmCrawl.getFilmFromName(search);
 
 	res.render('search', {
@@ -22,9 +25,22 @@ router.get('/:search', async (req, res) => {
 
 router.get('/film/:id', async (req, res) => {
 	const { id } = req.params;
+	console.log('view', id);
+
 	const data = await FilmCrawl.getFilmData(id);
+	const comments = await Film.getFilmComment(id);
+
+	data.comments = comments;
 
 	res.render('film-view', data);
+});
+
+router.post('/film/comment', async (req, res) => {
+	const { comment, id } = req.body;
+	console.log(comment, id);
+	await Film.addFilmComment(id, comment);
+
+	res.send(200);
 });
 
 module.exports = router
